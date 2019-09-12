16e3cc35d961999b362941b18948e6e89d9e46f3
Jenny Vossman <jenvoss17@gmail.com>
2019-09-12 17:59:48 -0500

    heroku3

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.DB_PASSWORD,
+    password: "Sweetheart!17",
     database: "burgers_db"
 });
 // Connect to the database
